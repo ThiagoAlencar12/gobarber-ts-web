a5adfdc7cb0711a6bfc4cde16ac7a3ecaece0d33
ThiagoAlencar12 <thiagofalencar12@gmail.com>
2020-05-14 15:26:09 -0300

    Login screen style complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>GoBarber TS</h1>;
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
